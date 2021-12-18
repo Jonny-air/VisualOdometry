7eca7a7560d3780a93fe1db4af9618f0cc2119b5
JeremyWanner <43452256+wannerje@users.noreply.github.com>
2021-12-18 22:08:42 +0100

    Update Initialization.m

--- Initialization.m
+++ Initialization.m
@@ -1,102 +1,32 @@
-%% Setup
+function [S_init,T_WC_init] = Initialization(frame1,frame2, K)
+%INITIALIZATION Initialize the states and pose (with respect to worldframe (1st frame)) using 2 initial frames (1st and 3rd suggested)
+%   -keypoints detection (harris) and matching (KLT) 
+%   -Calculate relative pose (8-points or 5-points) and triangulate 3D point cloud and outlier rejection (RANSAC)
+% Kp: keypoints count; 
+% P_0 [2 x Kp]: keypoints;
+% X_0 [3 x Kp]: 3D Landmarks (relative tp P0); 
+% Pose [R|T]: pose with respect to worldframe; 
+% R: relative rotation matrix (to express point from frame 2 to frame 1); 
+% T: relative translation (to express point from frame 2 to frame 1);
 
-ds = 2; % 0: KITTI, 1: Malaga, 2: parking
-parking_path = '../parking';
-bootstrap_frames = [1 3];
+% Get camera intrinsic parameters object
+intrinsics = getIntrinsicsCam(K);
 
-if ds == 0
-    % need to set kitti_path to folder containing "05" and "poses"
-    assert(exist('kitti_path', 'var') ~= 0);
-    ground_truth = load([kitti_path '/poses/05.txt']);
-    ground_truth = ground_truth(:, [end-8 end]);
-    last_frame = 4540;
-    K = [7.188560000000e+02 0 6.071928000000e+02
-        0 7.188560000000e+02 1.852157000000e+02
-        0 0 1];
-elseif ds == 1
-    % Path containing the many files of Malaga 7.
-    assert(exist('malaga_path', 'var') ~= 0);
-    images = dir([malaga_path ...
-        '/malaga-urban-dataset-extract-07_rectified_800x600_Images']);
-    left_images = images(3:2:end);
-    last_frame = length(left_images);
-    K = [621.18428 0 404.0076
-        0 621.18428 309.05989
-        0 0 1];
-elseif ds == 2
-    % Path containing images, depths and all...
-    assert(exist('parking_path', 'var') ~= 0);
-    last_frame = 598;
-    K = load([parking_path '/K.txt']);
-     
-    ground_truth = load([parking_path '/poses.txt']);
-    ground_truth = ground_truth(:, [end-8 end]);
-else
-    assert(false);
+% Find keypoints in first frame
+keypoints_frame1 = findInitialKeypoints(frame1, K);
+
+% use KLT to track keypoints to next frame
+[keypoints_frame1,keypoints_frame2] = trackKeypointsKLT(keypoints_frame1, frame1, frame2);
+
+% Estimate pose between the two initial frames
+[R_init,T_init] = poseP3pRansac(keypoints_frame1,keypoints_frame2,K,intrinsics);
+T_WC_init = [R_init,T_init'];
+
+
+% Triangulate world points
+X_initial = triangulateInitialLandmark(keypoints_frame1,keypoints_frame2,intrinsics,R_init,T_init);
+
+% Find new candidates and return current pose < S = [P,X,C,F,Tau] >
+S_init = findNewCandidateKeypoints(frame1,frame2,keypoints_frame1,keypoints_frame2,X_initial, T_WC_init, true);
+
 end
-
-%% Bootstrap
-% need to set bootstrap_frames
-if ds == 0
-    img0 = imread([kitti_path '/05/image_0/' ...
-        sprintf('%06d.png',bootstrap_frames(1))]);
-    img1 = imread([kitti_path '/05/image_0/' ...
-        sprintf('%06d.png',bootstrap_frames(2))]);
-elseif ds == 1
-    img0 = rgb2gray(imread([malaga_path ...
-        '/malaga-urban-dataset-extract-07_rectified_800x600_Images/' ...
-        left_images(bootstrap_frames(1)).name]));
-    img1 = rgb2gray(imread([malaga_path ...
-        '/malaga-urban-dataset-extract-07_rectified_800x600_Images/' ...
-        left_images(bootstrap_frames(2)).name]));
-elseif ds == 2
-    img0 = rgb2gray(imread([parking_path ...
-        sprintf('/images/img_%05d.png',bootstrap_frames(1))]));
-    img1 = rgb2gray(imread([parking_path ...
-        sprintf('/images/img_%05d.png',bootstrap_frames(2))]));
-else
-    assert(false);
-end
-
-%% Continuous operation
-% range = (bootstrap_frames(2)+1):last_frame;
-% for i = range
-%     fprintf('\n\nProcessing frame %d\n=====================\n', i);
-%     if ds == 0
-%         image = imread([kitti_path '/05/image_0/' sprintf('%06d.png',i)]);
-%     elseif ds == 1
-%         image = rgb2gray(imread([malaga_path ...
-%             '/malaga-urban-dataset-extract-07_rectified_800x600_Images/' ...
-%             left_images(i).name]));
-%     elseif ds == 2
-%         image = im2uint8(rgb2gray(imread([parking_path ...
-%             sprintf('/images/img_%05d.png',i)])));
-%     else
-%         assert(false);
-%     end
-%     % Makes sure that plots refresh.    
-%     pause(0.01);
-%     
-%     prev_img = image;
-% end
-
-
-%% Initialisation
-
-[S_init, T_WC_init] = Initialization(img0,img1,K);
-
-p = S_init.P;
-c = S_init.C;
-
-figure(1);
-imshow(img1);
-hold on;
-plot(p(1,:), p(2,:), 'rx', 'Linewidth', 2);
-plot(c(1,:), c(2,:), 'gx', 'Linewidth', 2);
-hold off
-
-%% Continuous operation
-%Pose = []
-%for i = 3:end
-%S_i+1, Pose_i+1 = ProcessFrame(S_i, Frame_i, frame_i+1)
-%Pose append Pose_new
